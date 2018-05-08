ee604836787a0df643a2336f777c92c0a7a81132
Dog fate <36992679+zhangfangxiang@users.noreply.github.com>
2018-05-08 17:27:52 +0800

    day10

---
+++ 5day/js.js
@@ -0,0 +1,3 @@
+document.getElementById('resume').onclick=function(){
+	console.log(document.getElementById("btn").innerHTML="篮球");
+}
---
+++ 5day/js/js_1.js
@@ -0,0 +1,6 @@
+var a=傻逼; 
+alert("a");
+
+// document.getElementById('resume').onclick=function(){
+// 	console.log(足球，篮球，保龄球);
+// };
