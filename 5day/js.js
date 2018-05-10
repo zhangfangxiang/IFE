ff982aae54965f46706299cffe054df1cdf127d0
Dog fate <36992679+zhangfangxiang@users.noreply.github.com>
2018-05-10 15:01:47 +0800

    day5

--- 5day/js.js
+++ 5day/js.js
@@ -1,3 +1,19 @@
 document.getElementById('resume').onclick=function(){
 	console.log(document.getElementById("btn").innerHTML="篮球");
 }
+function time(){
+        	var t=new Date().getHours();
+        	//console.log(t);
+        	if(0<t&&t<=8){
+        		alert("早上好");
+        	}else if(8<t&&t<=11){
+        		alert("上午好");
+	    	}else if(11<t&&t<=13){
+	    		alert("中午好")
+	    	}else if(13<t&&t<=17){
+	    		alert("下午好")
+	    	}else{
+	    		alert("晚上好")
+	    	}
+        }
+        time();
